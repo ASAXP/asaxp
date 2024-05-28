dc1504e9584fd3beec1d8eb5cda1ea59b26a226f
pglibrary80 <pglibrary80@gmail.com>
2024-05-28 22:20:01 +0900

    expressapp && husky

---
+++ packages/backend/express-app/src/app.ts
@@ -0,0 +1,9 @@
+// import express from "express";
+// import helmet from "helmet";
+// import * as path from "path";
+
+// const app = express();
+
+// app.use(helmet());
+
+console.log("hello express");
