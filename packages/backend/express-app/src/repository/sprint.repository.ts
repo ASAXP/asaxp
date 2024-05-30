c583fd78c1ac44e53e07fa07f92fb72fee91d0f5
pglibrary80 <pglibrary80@gmail.com>
2024-05-30 20:08:16 +0900

    fix: tsconfig

---
+++ packages/backend/express-app/src/repository/sprint.repository.ts
@@ -0,0 +1,17 @@
+import {} from "@asaxp/types";
+
+const SQL_SELECT = `
+  SELECT 
+  id,
+  name,
+  description,
+  cycle,
+  begin_date,
+  end_date,
+  updated_at,
+  created_at
+  FROM
+  sprints
+`;
+
+// const sprintFindAllRepo: Promise<Sprint[]> = () => {};
