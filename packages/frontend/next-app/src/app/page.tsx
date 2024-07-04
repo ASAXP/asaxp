b34d95c8c8152449d94a7a180873967ea3ffe0c3
pglibrary80 <pglibrary80@gmail.com>
2024-07-04 19:46:13 +0900

    docs: dependency cleanup

--- packages/frontend/next-app/src/app/page.tsx
+++ packages/frontend/next-app/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import { Button } from "antd";
 
 export default function Home() {
   return (
     <main className="">
       <Link href="/story">story page link</Link>
+      <Button type="primary">antdButton</Button>
     </main>
   );
 }
